3c56b502eb7d075f91d85c202a30214d3cb4d8b9
Sofia Cordoba <Soficordoba@live.com>
2025-11-10 19:19:47 -0300

    Implemented first entity and refactored logic to base class Entity Added UnitTest

--- Wpm.Management.Domain.Tests/UnitTest1.cs
+++ Wpm.Management.Domain.Tests/UnitTest1.cs
@@ -1,10 +1,17 @@
+using Wpm.Management.Domain;
+
 namespace Wpm.Management.Domain.Tests;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void Pet_must_be_equal()
     {
+        var id = Guid.NewGuid();
 
+        var pet1 = new Pet() { Id = id };
+        var pet2 = new Pet() { Id = id };
+
+        Assert.True(pet1 == pet2);
     }
 }
---
+++ Wpm.Management.Domain/Pet.cs
@@ -0,0 +1,6 @@
+﻿namespace Wpm.Management.Domain;
+public class Pet : Entity
+{
+    public string Name { get; set; }
+    public int Age { get; set; }    
+}
