71fc953f9f63618d32e2f89d3c40c47405934880
Sofia Cordoba <Soficordoba@live.com>
2025-11-23 10:16:37 -0300

    Implemented business rule at Pet entity about WeightClass depending on it's weight and sex. Added tests.

--- Wpm.Management.Domain.Tests/UnitTest1.cs
+++ Wpm.Management.Domain.Tests/UnitTest1.cs
@@ -12,8 +12,11 @@ public class UnitTest1
     public void Pet_must_be_equal()
     {
         var id = Guid.NewGuid();
-        var pet1 = new Pet(id, "Pupi", 10, new Weight(2.5m), "Brown", Pet.SexOfPet.Female);
-        var pet2 = new Pet(id, "Tito", 15, new Weight(3.5m), "Black", Pet.SexOfPet.Male);
+        var breedService = new FakeBreedService();
+        var breedId = new BreedId(breedService.breeds[0].Id, breedService);
+
+        var pet1 = new Pet(id, "Pupi", 10, "Brown", SexOfPetEnum.Female, breedId);
+        var pet2 = new Pet(id, "Tito", 15, "Black", SexOfPetEnum.Male, breedId);
 
         Assert.True(pet1 == pet2);
     }
@@ -40,9 +43,20 @@ public class UnitTest1
     public void BreedId_must_be_equal()
     {
         var breedService = new FakeBreedService();
-        var b1 = breedService.breeds.First().Id;
+        var b1 = breedService.breeds[0].Id;
         var breedId = new BreedId(b1, breedService);
 
         Assert.NotNull(breedId);
+    }
+
+    [Fact]
+    public void WeightClass_must_be_ideal()
+    {
+        var breedService = new FakeBreedService();
+        var breedId = new BreedId(breedService.breeds[0].Id, breedService);
+        var pet = new Pet(Guid.NewGuid(), "Test", 9, "Gray", SexOfPetEnum.Male, breedId);
+        pet.SetWeight(9.5m, breedService);
+
+        Assert.True(pet.WeightClass == WeightClassEnum.Underweight);
     }
 }
--- Wpm.Management.Domain/Pet.cs
+++ Wpm.Management.Domain/Pet.cs
@@ -5,23 +5,58 @@ public class Pet : Entity
 {
     public string Name { get; init; }
     public int Age { get; init; }    
-    public Weight Weight { get; init; }
+    public Weight Weight { get; private set; }
     public string Color { get; init; }
-    public SexOfPet SexPet { get; init; }
+    public SexOfPetEnum SexOfPet { get; init; }
     public BreedId BreedId { get; init; }
-    public Pet(Guid id, string name, int age, Weight weight, string color, SexOfPet sexPet)
+    public WeightClassEnum WeightClass { get; private set; }
+    public Pet(Guid id, string name, int age, string color, SexOfPetEnum sexPet, BreedId breedId)
     {
         Id = id;
         Name = name;
         Age = age;
-        Weight = weight;
         Color = color;
-        SexPet = sexPet;
+        SexOfPet = sexPet;
+        BreedId = breedId;
     }
 
-    public enum SexOfPet
+    public void SetWeight(Weight weight, IBreedService breedService)
     {
-        Male,
-        Female
+        Weight = weight;
+        SetWeightClass(breedService);
     }
+
+    private void SetWeightClass(IBreedService breedService)
+    {
+        var desiredBreed = breedService.GetBreed(BreedId.Value);
+        var (from, to) = SexOfPet switch
+        {
+            SexOfPetEnum.Male => (desiredBreed.MaleIdealWeightRange.From, desiredBreed.MaleIdealWeightRange.To),
+            SexOfPetEnum.Female => (desiredBreed.FemaleIdealWeightRange.From, desiredBreed.FemaleIdealWeightRange.To),
+            _ => throw new NotImplementedException()
+        };
+
+        var weightClass = Weight.Value switch
+        {
+            _ when Weight.Value < from => WeightClassEnum.Underweight,
+            _ when Weight.Value > to => WeightClassEnum.Overweight,
+            _ => WeightClassEnum.Ideal
+        };
+
+        WeightClass = weightClass;
+    }
+}
+
+public enum SexOfPetEnum
+{
+    Male,
+    Female
+}
+
+public enum WeightClassEnum
+{
+    Unknown,
+    Ideal,
+    Underweight,
+    Overweight
 }
